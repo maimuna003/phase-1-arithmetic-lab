6529588e4691bc78ffc6bdd0ef2c3f8633cf5223
maimuna <mohamudmaimuna003@gmail.com>
2023-06-18 19:24:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 62;
+const num2 = 1;
+const multiply = num1 * num2;
+
+const random = Number.isInteger(Math.random() * 10) + 1;
+console.log(random);
+
+const num3 = 25;
+const num4 = 7;
+const mod = num3 % num4;
+console.log(mod);
+
+const max = Math.max((1, 2, 3, 4, 20));
